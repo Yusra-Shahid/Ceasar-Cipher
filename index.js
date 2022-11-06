54bb8c3ae038cbe21a706eae178b326c74754354
yusra <yusrashahid111@gmail.com>
2022-11-06 18:03:18 +0500

    all done

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 const prompt = require("prompt-sync")({sigint: true});
 
-let text= " ";
-text =prompt("text : ");
-var Lock= parseInt(prompt("Lock: "))
+let plain_text= " ";
+plain_text =prompt("text : ");
+var Key= parseInt(prompt("Key: "))
 let en='';
 i=0;
 charCode=0;
 
-for(i=0; i<text.length;i++)
+for(i=0; i<plain_text.length;i++)
 {
-    charCode = text[i].charCodeAt();
+    charCode = plain_text[i].charCodeAt();
     if (charCode >= 97 && charCode <= 122){
-        charCode = 97 + (charCode - 97 + Lock) % 26;
+        charCode = 97 + (charCode - 97 + Key) % 26;
         //charCode += k % 26;
     }
     // else if(charCode > 122){
@@ -23,5 +23,5 @@ for(i=0; i<text.length;i++)
     }
     en += String.fromCharCode(charCode);
 }
-    console.log(`Encrypted Text: ${en}`);
+    console.log(`Now convert into Ceasar Text: ${en}`);
 
